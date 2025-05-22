b8c824caf683f8371765b4ecf0d2ce0e410d1b25
Ítalo Farias <italo.farias@devoteam.com>
2025-05-22 18:36:36 +0100

    fixed dependency injection issue

--- MovieRental/Program.cs
+++ MovieRental/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEntityFrameworkSqlite().AddDbContext<MovieRentalDbContext>();
 
-builder.Services.AddSingleton<IRentalFeatures, RentalFeatures>();
+//change to scoped so it can be injected in the RentalController (since dbContext is scoped)
+builder.Services.AddScoped<IRentalFeatures, RentalFeatures>();
+//builder.Services.AddSingleton<IRentalFeatures, RentalFeatures>();
 
 var app = builder.Build();
 
@@ -25,9 +27,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var client = new MovieRentalDbContext())
+//there is no need to instantiate the dbcontext here
+//using (var client = new MovieRentalDbContext())
+//{
+//	client.Database.EnsureCreated();
+//}
+
+using (var scope = app.Services.CreateScope())
 {
-	client.Database.EnsureCreated();
+    var dbContext = scope.ServiceProvider.GetRequiredService<MovieRentalDbContext>();
+    dbContext.Database.EnsureCreated();
 }
 
 app.Run();
